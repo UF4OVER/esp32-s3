d033fcdf6fd6c583d836a9aacb38d536cc6eed8f
heping <3397499417@qq.com>
2024-08-03 15:33:27 +0800

    diandeng

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
 import time
 
-import machine
-import neopixel
+from machine import DAC, Pin
 
-rgb = neopixel.NeoPixel(machine.Pin(48), 1)
-
+dac_pin = Pin(26, Pin.OUT)
+dac = DAC(dac_pin)
 while True:
-    # 渐变多层次呼吸灯
-    for i in range(0, 255):
-        rgb.fill((i, 0, 0))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((0, i, 0))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((0, 0, i))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((255 - i, 0, 0))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((0, 255 - i, 0))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((0, 0, 255 - i))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((255 - i, 255 - i, 0))
-        rgb.write()
-        time.sleep(0.01)
-
-
-    for i in range(0, 255):
-        rgb.fill((255 - i, 0, 255 - i))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((0, 255 - i, 255 - i))
-        rgb.write()
-        time.sleep(0.01)
-
-    for i in range(0, 255):
-        rgb.fill((255 - i, 255 - i, 255 - i))
-        rgb.write()
-        time.sleep(0.01)
+    # 呼吸效果
+    for i in range(0, 1023):
+        dac.write(i)
+        time.sleep_ms(10)
+    for i in range(1023, 0, -1):
+        dac.write(i)
+        time.sleep_ms(10)
