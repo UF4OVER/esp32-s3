1f9161049ad7e19d8a5c6e71d94f112487f3ac2c
heping <3397499417@qq.com>
2024-08-03 11:33:07 +0800

    diandeng

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ import machine
 import neopixel
 
 rgb = neopixel.NeoPixel(machine.Pin(48), 1)
-rgb.fill((255, 0, 0))
-rgb.write()
+# 渐变呼吸灯
+while True:
+    for i in range(0, 255):
+        rgb.fill((i, 255 - i, 0))
+        rgb.write()
+    for i in range(0, 255):
+        rgb.fill((255 - i, 0, i))
+        rgb.write()
+    for i in range(0, 255):
+        rgb.fill((0, i, 255 - i))
+        rgb.write()
